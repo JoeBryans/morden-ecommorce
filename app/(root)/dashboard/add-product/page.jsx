7fe2047afb073f667097a6e1589fda6273c48c40
Joebryan <emekejohua12@gmail.com>
2025-09-09 04:47:04 -0700

    error fixed from primal client

--- app/(root)/dashboard/add-product/page.jsx
+++ app/(root)/dashboard/add-product/page.jsx
@@ -1,6 +1,4 @@
-"use client";
-import AddProduct from "@/components/form/AddProduct";
-import { Button } from "@/components/ui/button";
+
 import Category from "@/components/form/category";
 import NavBar from "../NavBar";
 import { NewProduct } from "@/components/form/newProduct";
@@ -10,10 +8,10 @@ const page = () => {
       <NavBar seachBar={false} />
       <div className="flex  min-h-[110vh] mt-3">
         <div className="max-w-full overflow-hidden   w-[90%] flex flex-col items-center  mx-auto  ">
-         <div className="flex items-center max-w-5xl w-full justify-between mb-5">
+          <div className="flex items-center max-w-5xl w-full justify-between mb-5">
             <h2 className="text-center font-bold text-3xl">Create Product</h2>
             <div className="flex justify-end "><Category /></div>
-         </div>
+          </div>
 
 
           {/* <AddProduct /> */}
